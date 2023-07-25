50a478b9fb1bc41d90cf2dea0e15781fe718226f
ElFadiliSalwa <89156199+ElFadiliSalwa@users.noreply.github.com>
2023-07-25 23:31:07 +0100

    Create main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+
+void display_prompt();
+char *read_input();
+void execute_command(char *command);
+
+#endif
